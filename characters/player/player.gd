021add605a730328385e1aa2d1f0c8de25885389
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-05-09 13:26:57 -0400

    update movement

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,27 +1,50 @@
 extends CharacterBody3D
 
+@onready var head = $Head
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var current_speed: float = 5.0
+
+@export var walking_speed: float = 5.0
+@export var sprinting_speed: float = 8.0
+@export var crouching_speed: float = 3.0
+@export var mouse_sens: float = 0.3
+
+
+var jump_velocity: float = 4.5
+var lerp_speed: float = 10.0
+var direction: Vector3 = Vector3.ZERO
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-deg_to_rad(event.relative.x * mouse_sens))
+		head.rotate_x(-deg_to_rad(event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprinting_speed
+	else:
+		current_speed = walking_speed
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
