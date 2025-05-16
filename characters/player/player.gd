3dc2f284f7f4efb4e55682f379f32d5424040b06
Demonik_bear <Ayman10alz@gmail.com>
2025-05-16 16:49:44 +0300

    Added multi-jump, gravity controller, coyote jump

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -8,9 +8,12 @@ var current_speed: float = 5.0
 @export var sprinting_speed: float = 9.0
 @export var crouching_speed: float = 3.0
 @export var mouse_sens: float = 0.3
+@export var Jump_Max: int = 1
+@export var initialGravity: float = 2
+@export var Jumptime: float = 0.8
 
-
-var jump_velocity: float = 6
+var Jump_amount: int = 1
+var jump_velocity: float = 7
 var lerp_speed: float = 10.0
 var direction: Vector3 = Vector3.ZERO
 
@@ -28,7 +31,7 @@ func _input(event):
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("crouch"):
 		current_speed = crouching_speed if is_on_floor() else current_speed
-		head.position.y = lerp(head.position.y, 1.8 + crouching_depth, delta * lerp_speed)
+		head.position.y = lerp(head.position.y, 1.6 + crouching_depth, delta * lerp_speed)
 		$StandingShape.disabled = true
 		$CrouchingShape.disabled = false
 	elif !$StandingCast.is_colliding():
@@ -41,11 +44,18 @@ func _physics_process(delta: float) -> void:
 			current_speed = walking_speed
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if Jump_amount:
+			#Makes a timer connecting to the variable to the Function
+			get_tree().create_timer(Jumptime).timeout.connect(jumptimeout)
+		
+		velocity += get_gravity() * initialGravity * delta 
+	else:
+		Jump_amount = Jump_Max
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and Jump_amount > 0:
 		velocity.y = jump_velocity
+		Jump_amount += -1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -59,3 +69,7 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
+
+#Timer calls this function when its timed out
+func jumptimeout():
+	Jump_amount += -1
