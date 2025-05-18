e102fdd0c3f49637cd60f5ccee0bc71420a721a4
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-05-18 13:08:49 -0400

    more formatting

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -62,9 +62,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	direction = lerp(direction, 
-						(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), 
-						delta * lerp_speed)
+	direction = lerp(direction,
+		(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),
+		delta * lerp_speed)
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
